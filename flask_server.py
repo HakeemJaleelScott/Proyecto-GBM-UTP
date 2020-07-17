359ca244897cd3d4e5ff2d658169a60381d98d0d
Hakeem Jaleel Scott <hakeemscott30@gmail.com>
2020-07-17 04:00:14 -0500

    modificacion en el servidor: metodos add,create y read. añadido: metodo update(por completar)

--- flask_server.py
+++ flask_server.py
@@ -4,27 +4,27 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 
 prueba = Flask(__name__)
-#prueba.debug = True
+# prueba.debug = True
 prueba.config['MONGO_URI'] = 'mongodb://localhost:27017/prueba'
 mongo = PyMongo(prueba)
-
 
 
 """We then use the route() decorator to tell Flask what URL should trigger our function.
 establece la ruta dentro del servidor donde consultar la data
 """
 
+
 @prueba.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
         uid = request.form['id']
         try:
-            data = mongo.db.Colection_examp_produc.find_one({'_id': uid})
-            return redirect(url_for('add', uid=data['_id'],  name=data['name']))
+           data = mongo.db.Colection_examp_produc.find_one({'_id': uid})
+           return redirect(url_for('read', uid=data['_id']))
 
         except:
 
-            return redirect(url_for('create', user=uid))
+             return redirect(url_for('create', user=uid))
 
     else:
         return render_template('start.html')
@@ -94,8 +94,8 @@ def add(uid, name):
         return render_template('add.html', uid=uid, name=name)
 
 
-@prueba.route('/read', methods=['POST', 'GET'])
-def read():
+@prueba.route('/read/<uid>', methods=['POST', 'GET'])
+def read(uid):
     if request.method == 'POST':
         uid = request.form['id']
         data = mongo.db.Colection_examp_produc.find_one({'_id': uid})
@@ -103,7 +103,8 @@ def read():
         return render_template('read.html', data=data)
 
     else:
-        return render_template('start.html')
+        data = mongo.db.Colection_examp_produc.find_one({'_id': uid})
+        return render_template('read.html', data=data)
 
 
 @prueba.route('/build')
@@ -111,9 +112,39 @@ def build():
     return render_template('EUREKA.html')
 
 
-@prueba.route('/update')
-def update():
-    return 0
+@prueba.route('/update/<uid>/<index>', methods=['POST', 'GET'])
+def update(uid, index):
+    
+    index = int(index)
+    if request.method == 'POST':
+        model = request.form['model']
+        devType = request.form['devType']
+        serial = request.form['serial']
+        sysName = request.form['sysName']
+        location = request.form['location']
+        env = request.form['env']
+        config = request.form['config']
+        os = request.form['os'] 
+        #TODO
+        #A ver que pex aqui... :'D
+        mongo.db.Colection_examp_produc.update_one({'_id': uid}, {'$set': {'sistema.$.[index]':
+                                                                        {'modelo': model,
+                                                                         'tipo': devType,
+                                                                         'serie': serial,
+                                                                         'sysName': sysName,
+                                                                         'ubicacion': location,
+                                                                         'ambiente': env,
+                                                                         'config': config,
+                                                                         'os': os},
+                                                    
+                                                                        }
+                                                              },
+                                               # {arrayFilters:index,upsert:False}
+                                               )
+        return redirect(url_for('read', uid=uid))
+    else:
+        data = mongo.db.Colection_examp_produc.find_one({'_id': uid})
+        return render_template('update.html', data=data, sistema=index)
 
 
 @prueba.route('/delete')
