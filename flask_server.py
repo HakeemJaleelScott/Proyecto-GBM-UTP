af2e1fef862488bfa68389bcd184f994f86700bb
Hakeem Jaleel Scott <hakeemscott30@gmail.com>
2020-11-03 21:27:21 -0500

    nueva rama en git y modificaciones finales del proyecto

--- flask_server.py
+++ flask_server.py
@@ -3,10 +3,10 @@ import json
 from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 
-prueba = Flask(__name__)
-# prueba.debug = True
-prueba.config['MONGO_URI'] = 'mongodb://localhost:27017/prueba'
-mongo = PyMongo(prueba)
+app = Flask(__name__)
+# app.debug = True
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/GBM'
+mongo = PyMongo(app)
 
 
 """We then use the route() decorator to tell Flask what URL should trigger our function.
@@ -14,17 +14,17 @@ establece la ruta dentro del servidor donde consultar la data
 """
 
 
-@prueba.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def home():
     #flag = True
     if request.method == 'POST':
         uid = request.form['id']
-        data =list(mongo.db.Colection_examp_produc.find({'name':{'$regex': uid, '$options':'i'}}))
+        data =list(mongo.db.ClientesPA.find({'name':{'$regex': uid, '$options':'i'}}))
         if data:
             print ('found by name')
             return render_template('show.html', data=data) 
         elif not data:
-            data = list(mongo.db.Colection_examp_produc.find({'_id':{'$regex': uid}}))
+            data = list(mongo.db.ClientesPA.find({'_id':{'$regex': uid}}))
             if data:
                 print('found by id')
                 return render_template('show.html', data=data)
@@ -40,21 +40,21 @@ def home():
         return render_template('start.html')
 
 
-@prueba.route('/create', methods=['POST', 'GET'])
+@app.route('/create', methods=['POST', 'GET'])
 def create():
     if request.method == 'POST':
         uid = request.form['id']
         name = request.form['name']
 
-        mongo.db.Colection_examp_produc.insert_one({'_id': uid, 'name': name })
+        mongo.db.ClientesPA.insert_one({'_id': uid, 'name': name })
 
-        #mongo.db.Colection_examp_produc.find_one({'_id': uid})
+        #mongo.db.ClientesPA.find_one({'_id': uid})
         return redirect(url_for('create'))
     else:
         return render_template('create.html')
 
 
-@prueba.route('/add/<uid>/<name>', methods=['POST', 'GET'])
+@app.route('/add/<uid>/<name>', methods=['POST', 'GET'])
 def add(uid, name):
     if request.method == 'POST':
         uid = uid
@@ -68,7 +68,7 @@ def add(uid, name):
         config = request.form['config']
         os = request.form['os']
 
-        mongo.db.Colection_examp_produc.update({'_id': uid}, {'$push': {'sistema':
+        mongo.db.ClientesPA.update({'_id': uid}, {'$push': {'sistema':
                                                                         {'modelo': model,
                                                                          'tipo': devType,
                                                                          'serie': serial,
@@ -86,26 +86,57 @@ def add(uid, name):
     else:
         return render_template('add.html', uid=uid, name=name)
 
+@app.route('/addsw/<uid>/<name>', methods=['POST', 'GET'])
+def addsw(uid, name):
+    if request.method == 'POST':
+        uid = uid
+        name = name
+        plataforma = request.form['plataforma']
+        version = request.form['version']
+        SO = request.form['SO']
+        solucion = request.form['solucion']
+        location = request.form['location']
+        env = request.form['env']
+        config = request.form['config']
+        os = request.form['os']
 
-@prueba.route('/read/<uid>', methods=['POST', 'GET'])
+        mongo.db.ClientesPA.update({'_id': uid}, {'$push': {'SW':
+                                                                        {'plataforma': plataforma,
+                                                                         'version': version,
+                                                                         'SO': SO,
+                                                                         'solucion': solucion,
+                                                                         '': location,
+                                                                         'ambiente': env,
+                                                                         'config': config,
+                                                                         'os': os}
+
+                                                                        }
+                                                              }
+                                               )
+        return redirect(url_for('add', uid=uid, name=name))
+
+    else:
+        return render_template('add.html', uid=uid, name=name)
+
+@app.route('/read/<uid>', methods=['POST', 'GET'])
 def read(uid):
     if request.method == 'POST':
         uid = request.form['id']
-        data = mongo.db.Colection_examp_produc.find_one({'_id': uid})
+        data = mongo.db.ClientesPA.find_one({'_id': uid})
 
         return render_template('read.html', data=data)
 
     else:
-        data = mongo.db.Colection_examp_produc.find_one({'_id': uid})
+        data = mongo.db.ClientesPA.find_one({'_id': uid})
         return render_template('read.html', data=data)
 
 
-@prueba.route('/build')
+@app.route('/build')
 def build():
     return render_template('EUREKA.html')
 
 
-@prueba.route('/update/<uid>/<index>', methods=['POST', 'GET'])
+@app.route('/update/<uid>/<index>', methods=['POST', 'GET'])
 def update(uid, index):
     
     index1 = int(index)
@@ -121,7 +152,7 @@ def update(uid, index):
         os = request.form['os'] 
         index
     
-        mongo.db.Colection_examp_produc.update_one({'_id': uid}, {'$set': {'sistema.'+index2+'.modelo': model,
+        mongo.db.ClientesPA.update_one({'_id': uid}, {'$set': {'sistema.'+index2+'.modelo': model,
                                                                          'sistema.'+index2+'.tipo': devType,
                                                                          'sistema.'+index2+'.serie': serial,
                                                                          'sistema.'+index2+'.sysName': sysName,
@@ -134,14 +165,14 @@ def update(uid, index):
                                                )
         return redirect(url_for('read', uid=uid))
     else:
-        data = mongo.db.Colection_examp_produc.find_one({'_id': uid})
+        data = mongo.db.ClientesPA.find_one({'_id': uid})
         return render_template('update.html', data=data, sistema=index1)
 
 
-@prueba.route('/delete')
+@app.route('/delete')
 def delete():
     return 0
 
 
 if __name__ == "__main__":
-    prueba.run(host='127.0.0.1', port=80, debug=True)
+    app.run(host='127.0.0.1', port=80, debug=True)
